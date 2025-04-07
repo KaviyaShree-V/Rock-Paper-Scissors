53dcb201e27468803d7e12c6b26c110c153400ef
KaviyaShree-V <karthikavi1976@gmail.com>
2025-04-07 22:44:31 +0530

    Maim Function

---
+++ RPS.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class RPS {
+    public static void main(String[] args) { // Change method name to 'main'
+
+        // Create an instance of RPSG (Rock Paper Scissors Game)
+        RPSG game = new RPSG();
+        // Start the game call the method startgame
+        game.startGame();
+    }
+}
